c0d915c23944bfbfff67620005f662054d8c80aa
Nico Saueressig <nico@nicosaueressig.de>
2014-01-17 22:46:30 +0100

    Added database support

--- blog.rb
+++ blog.rb
@@ -1,24 +1,35 @@
+require 'rubygems'
 require 'sinatra'
-get '/hi' do
-	"Hello World!\n"
+require 'mongo'
+
+db = Mongo::Connection.from_uri("mongodb://dev:penis@dharma.mongohq.com:10099/test1")['test1']
+
+#.new['test']
+
+
+get '/' do
+	telefonbuch = db['namen'].find()
+	result = telefonbuch.map{|document| "#{document['nachname']}, #{document['vorname']}"}.join("<br>")
+	result += '<a href="new">Neu</a>'
+	result
 end
 
-telefonbuch = {"Nico" => "Saueressig", "Johannes" => "Hofmeister"}
-telefonbuch.default = "Unbekannt"
-
-get '/kunde/' do
-	vorname = params[:name]
-	"Hallo, Herr #{telefonbuch[vorname]} \n"
+get '/new' do
+	'<form method="post" action="new">
+		<input name="vorname" type="text" placeholder="Vorname"></input><br>
+		<input name="nachname" type="text" placeholder="Nachname"></input>
+		<button>Speichern</button>
+	</form>'
 end
 
-
-get '/test/' do
-	"Dies ist ein Test\n"
-end
-
-post '/kunde/' do
+post '/new' do
      vorname = params[:vorname]
      nachname = params[:nachname]
-     telefonbuch[vorname] = nachname
-     "#{nachname}, #{vorname} wurde eingetragen!"
+     db['namen'].insert({:vorname=> vorname, :nachname=> nachname})
+     "#{nachname}, #{vorname} wurde eingetragen! <a href='new'>Neu</a> <a href='/'>Alle</a> "
 end
+
+
+
+
+
